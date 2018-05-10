0cead552c3f14428da8f647355311e3e6a09f1ba
lifeofmine <uccent@sina.com>
2018-05-10 17:32:46 +0800

    first commit java comeback

---
+++ src/puppy.java
@@ -0,0 +1,9 @@
+public class puppy {
+    public puppy(String name) {
+        System.out.println("小狗的名字是：" + name);
+    }
+
+    public static void main(String []args) {
+        puppy myPuppy = new puppy("tommy");
+    }
+}
