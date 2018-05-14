776804160beeaa8ab48bf17bea3066326b20b5f7
lifeofmine <uccent@sina.com>
2018-05-14 16:21:21 +0800

    java Character

---
+++ src/CharacterBox/Transfer.java
@@ -0,0 +1,9 @@
+package CharacterBox;
+
+public class Transfer {
+    public static void main(String args[]) {
+        Character c = 'a';
+        System.out.println("访问\"java教程\"");
+        System.out.println(Character.toUpperCase(c));
+    }
+}
