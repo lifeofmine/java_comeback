f6a8c87f6f9fa0a22379fa265a81d57f0df2beaf
lifeofmine <uccent@sina.com>
2018-05-17 18:16:14 +0800

    java http

---
+++ src/HttpDemo/HttpRequest.java
@@ -0,0 +1,51 @@
+package HttpDemo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HttpRequest {
+    public static void main(String[] args) {
+        String result = HttpRequest.sendGet("http://testapi.dahuasecurity.com/test.php", "user=1");
+        System.out.println(result);
+    }
+    public static String sendGet(String Url, String Param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = Url + "?" + Param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
