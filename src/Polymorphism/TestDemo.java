c405e69608e738c2763371e8b1931a9fea7cb798
lifeofmine <uccent@sina.com>
2018-05-23 15:08:55 +0800

    java polymorphism

---
+++ src/Polymorphism/TestDemo.java
@@ -0,0 +1,47 @@
+package Polymorphism;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        show(new Cat());
+        show(new Dog());
+
+        Animal a = new Cat();
+        a.eat();
+        Cat c = (Cat)a;
+        c.work();
+    }
+
+    public static void show(Animal a) {
+        a.eat();
+        if (a instanceof Cat) {
+            Cat c = (Cat)a;
+            c.work();
+        } else if (a instanceof Dog) {
+            Dog c = (Dog)a;
+            c.work();
+        }
+    }
+}
+
+abstract class Animal {
+    abstract void eat();
+}
+
+class Cat extends Animal {
+    public void eat() {
+        System.out.println("吃鱼");
+    }
+
+    public void work() {
+        System.out.println("抓老鼠");
+    }
+}
+
+class Dog extends Animal {
+    public void eat() {
+        System.out.println("吃骨头");
+    }
+    public void work() {
+        System.out.println("看家");
+    }
+}
