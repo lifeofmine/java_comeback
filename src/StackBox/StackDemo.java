ff521c8087f2c9860b8cf6615acfaa9bebfb3871
lifeofmine <uccent@sina.com>
2018-05-25 16:09:39 +0800

    java Vector

---
+++ src/StackBox/StackDemo.java
@@ -0,0 +1,34 @@
+package StackBox;
+import java.util.*;
+
+public class StackDemo {
+    static void showpush(Stack<Integer> st, int a) {
+        st.push(new Integer(a));
+        System.out.println("push(" + a + ")");
+        System.out.println("stack: " + st);
+    }
+
+    static void showpop(Stack<Integer> st) {
+        System.out.println("pop -> ");
+        Integer a = (Integer) st.pop();
+        System.out.println(a);
+        System.out.println("stack: " + st);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>();
+        System.out.println("stack: " + st);
+        showpush(st, 42);
+        showpush(st, 66);
+        showpush(st, 99);
+        showpop(st);
+        showpop(st);
+        showpop(st);
+        try {
+            showpop(st);
+        } catch (EmptyStackException e) {
+            System.out.println(e.getMessage());
+            System.out.println("empty stack");
+        }
+    }
+}
