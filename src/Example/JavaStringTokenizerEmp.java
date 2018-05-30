2b24e702674120b35b3f72ef7d871de857e947d0
lifeofmine <uccent@sina.com>
2018-05-30 14:14:41 +0800

    java ExampleString

---
+++ src/Example/JavaStringTokenizerEmp.java
@@ -0,0 +1,22 @@
+package Example;
+
+import java.util.StringTokenizer;
+
+public class JavaStringTokenizerEmp {
+    public static void main(String[] args) {
+        String str = "This is String, split by StringTokenizer, created by runoob";
+        StringTokenizer st = new StringTokenizer(str);
+
+        System.out.println("----- 通过空格分隔 -----");
+        while (st.hasMoreElements()) {
+            System.out.println(st.nextElement());
+        }
+
+        System.out.println("----- 通过逗号分隔 -----");
+        StringTokenizer st2 = new StringTokenizer(str, ",");
+
+        while (st2.hasMoreElements()) {
+            System.out.println(st2.nextElement());
+        }
+    }
+}
---
+++ src/Example/StringRegionMatch.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class StringRegionMatch {
+    public static void main(String[] args) {
+        String first_str = "Welcome to Microsoft";
+        String second_str = "I work with microsoft";
+        boolean match1 = first_str.regionMatches(11, second_str, 12, 9);
+        boolean match2 = first_str.regionMatches(true, 11, second_str, 12, 9);
+        System.out.println("区分大小写返回值：" + match1);
+        System.out.println("不区分大小写返回值：" + match2);
+    }
+}
