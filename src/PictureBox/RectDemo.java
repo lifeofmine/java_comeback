2b210de94419c4c1efd38247c101e05438164ae6
lifeofmine <uccent@sina.com>
2018-06-06 18:02:49 +0800

    java Picture

---
+++ src/PictureBox/RectDemo.java
@@ -0,0 +1,12 @@
+package PictureBox;
+
+public class RectDemo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
